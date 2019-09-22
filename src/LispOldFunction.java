b8efcbe5afb5d72a5bd8410301e4391a362a99c5
Ben Johnson-Laird <benjohnsonlaird@gmail.com>
2019-09-22 17:06:18 -0400

    Deleted

--- src/LispOldFunction.java
+++
@@ -1,9 +0,0 @@
-
-@FunctionalInterface
-public interface LispOldFunction {
-	
-	//The type of arg has to match exactly in the class instance 
-	LispList checkMod(LispAtom classInstance, LispAtom arg);
-
-}
-
